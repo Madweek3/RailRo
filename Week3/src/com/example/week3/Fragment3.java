9f6f4ed13fa72b09e146c2a1b8b5168ce3a6f724
Hyun Il Harry Shin <shuin3180@naver.com>
2015-01-20 18:31:52 +0900

    GOOGLE SEARCH API ADJUSTED
    need to be parsed

--- Week3/src/com/example/week3/Fragment3.java
+++ Week3/src/com/example/week3/Fragment3.java
@@ -1,14 +1,19 @@
 package com.example.week3;
- 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -24,21 +29,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 
-
 public class Fragment3 extends Fragment {
 	private View view;
-    EditText etResponse;
-    TextView tvIsConnected;
-    public Fragment3() {
+	EditText etResponse;
+	TextView tvIsConnected;
+
+	public Fragment3() {
 		// TODO Auto-generated constructor stub
 	}
-    @Override
+
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		view = inflater.inflate(R.layout.fragment3, container, false);
- 
-        // get reference to the views
+		
+		
+		
+		
+      
+		
+		// get reference to the views
         etResponse = (EditText) view.findViewById(R.id.etResponse);
         tvIsConnected = (TextView) view.findViewById(R.id.tvIsConnected);
  
@@ -52,71 +63,79 @@ public class Fragment3 extends Fragment {
         }
  
         // call AsynTask to perform network operation on separate thread
-        new HttpAsyncTask().execute("http://www.google.com/search?q=developer");
+        new HttpAsyncTask().execute("");
         
         return view;
     }
- 
-    public static String GET(String url){
-        InputStream inputStream = null;
-        String result = "";
-        try {
- 
-            // create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
- 
-            // make GET request to the given URL
-            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
- 
-            // receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
- 
-            // convert inputstream to string
-            if(inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
- 
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
- 
-        return result;
-    }
- 
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
- 
-        inputStream.close();
-        return result;
- 
-    }
- 
-    public boolean isConnected(){
-        ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Activity.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isConnected()) 
-                return true;
-            else
-                return false;   
-    }
-    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
- 
-            return GET(urls[0]);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getActivity().getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
-            etResponse.setText(result);
-       }
-    }
+
+	public static String GET(String my_url) {
+		URL url = null;
+		StringBuilder builder=null;
+		try {
+			url = new URL(
+				    "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
+				    + "q=+developer&userip=USERS-IP-ADDRESS");
+			URLConnection connection = url.openConnection();
+			String line;
+			builder = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			while((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+			JSONObject json = new JSONObject(builder.toString());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		InputStream inputStream = null;
+		String result = "";
+		
+		return builder.toString();
+	}
+
+	private static String convertInputStreamToString(InputStream inputStream)
+			throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(inputStream));
+		String line = "";
+		String result = "";
+		while ((line = bufferedReader.readLine()) != null)
+			result += line;
+
+		inputStream.close();
+		return result;
+
+	}
+
+	public boolean isConnected() {
+		ConnectivityManager connMgr = (ConnectivityManager) getActivity()
+				.getSystemService(Activity.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected())
+			return true;
+		else
+			return false;
+	}
+
+	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			
+			return GET(urls[0]);
+		}
+
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(getActivity().getBaseContext(), "Received!",
+					Toast.LENGTH_LONG).show();
+			etResponse.setText(result);
+		}
+	}
 }
-
-
