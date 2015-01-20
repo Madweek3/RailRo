327cb482ccaebc8b782dfdc26fcef15c7335175d
Hyun Il Harry Shin <shuin3180@naver.com>
2015-01-20 15:01:12 +0900

    HTML Request&Response able with trash
    but it gives trash html code

--- Week3/src/com/example/week3/Fragment3.java
+++ Week3/src/com/example/week3/Fragment3.java
@@ -1,25 +1,122 @@
 package com.example.week3;
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+
 
 public class Fragment3 extends Fragment {
 	private View view;
-	
-	
-	
-	
-	public Fragment3() {
+    EditText etResponse;
+    TextView tvIsConnected;
+    public Fragment3() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+
 		view = inflater.inflate(R.layout.fragment3, container, false);
-		
-		return view;
-	}
+ 
+        // get reference to the views
+        etResponse = (EditText) view.findViewById(R.id.etResponse);
+        tvIsConnected = (TextView) view.findViewById(R.id.tvIsConnected);
+ 
+        // check if you are connected or not
+        if(isConnected()){
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are conncted");
+        }
+        else{
+            tvIsConnected.setText("You are NOT conncted");
+        }
+ 
+        // call AsynTask to perform network operation on separate thread
+        new HttpAsyncTask().execute("http://www.google.com/search?q=developer");
+        
+        return view;
+    }
+ 
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+ 
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+ 
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+ 
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+ 
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+ 
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+ 
+        return result;
+    }
+ 
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+ 
+        inputStream.close();
+        return result;
+ 
+    }
+ 
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getActivity().getSystemService(Activity.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) 
+                return true;
+            else
+                return false;   
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+ 
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getActivity().getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            etResponse.setText(result);
+       }
+    }
 }
+
+
