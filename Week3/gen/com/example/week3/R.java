4406ef48a36706db015f221c4f2441a0f231f7cd
Hyun Il Harry Shin <shuin3180@naver.com>
2015-01-21 20:51:37 +0900

    GOOGLE SEARCH PAGE FINISH
    trying to add map api

--- Week3/gen/com/example/week3/R.java
+++ Week3/gen/com/example/week3/R.java
@@ -36,11 +36,15 @@ public final class R {
     }
     public static final class id {
         public static final int FrameLayout1=0x7f080000;
-        public static final int action_settings=0x7f08000a;
-        public static final int etResponse=0x7f080009;
+        public static final int action_settings=0x7f08000e;
+        public static final int desc=0x7f08000c;
+        public static final int f3_button_before=0x7f080009;
+        public static final int f3_button_next=0x7f08000a;
         public static final int frag1_textView=0x7f080006;
         public static final int fragment1=0x7f080005;
         public static final int fragment2=0x7f080007;
+        public static final int list=0x7f08000b;
+        public static final int name=0x7f08000d;
         public static final int pager=0x7f080004;
         public static final int tabPage1=0x7f080001;
         public static final int tabPage2=0x7f080002;
@@ -52,6 +56,7 @@ public final class R {
         public static final int fragment1=0x7f030001;
         public static final int fragment2=0x7f030002;
         public static final int fragment3=0x7f030003;
+        public static final int list_elem=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -59,6 +64,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int button_before=0x7f050004;
+        public static final int button_next=0x7f050003;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
--- Week3/src/com/example/week3/Fragment3.java
+++ Week3/src/com/example/week3/Fragment3.java
@@ -7,11 +7,13 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,93 +26,162 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 
 public class Fragment3 extends Fragment {
 	private View view;
-	EditText etResponse;
+	ListView listv;
 	TextView tvIsConnected;
+	Button btn_b;
+	Button btn_n;
+	String output;
+	String searcher = "developer";
+	String[] values_name = {};
+	String[] values_desc = {};
+	String[] search_string;
+	int page = 0;
 
 	public Fragment3() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		view = inflater.inflate(R.layout.fragment3, container, false);
-		
-		
-		
-		
-      
-		
-		// get reference to the views
-        etResponse = (EditText) view.findViewById(R.id.etResponse);
-        tvIsConnected = (TextView) view.findViewById(R.id.tvIsConnected);
- 
-        // check if you are connected or not
-        if(isConnected()){
-            tvIsConnected.setBackgroundColor(0xFF00CC00);
-            tvIsConnected.setText("You are conncted");
-        }
-        else{
-            tvIsConnected.setText("You are NOT conncted");
-        }
- 
-        // call AsynTask to perform network operation on separate thread
-        new HttpAsyncTask().execute("");
-        
-        return view;
-    }
 
-	public static String GET(String my_url) {
+		search_string = new String[2];
+		search_string[0] = searcher;
+		search_string[1] = Integer.toString(page);
+		try {
+			output = new HttpAsyncTask().execute(search_string).get();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ExecutionException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		// get reference to the views
+		listv = (ListView) view.findViewById(R.id.list);
+		tvIsConnected = (TextView) view.findViewById(R.id.tvIsConnected);
+		btn_b = (Button) view.findViewById(R.id.f3_button_before);
+		btn_n = (Button) view.findViewById(R.id.f3_button_next);
+		btn_b.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				try {
+					if (page != 0)
+						page -= 4;
+					search_string = new String[2];
+					search_string[0] = searcher;
+					search_string[1] = Integer.toString(page);
+					output = new HttpAsyncTask().execute(search_string).get();
+					if(page==0)
+						btn_b.setEnabled(false);
+					makeview(listv);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		btn_n.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				try {
+					page += 4;
+					search_string = new String[2];
+					search_string[0] = searcher;
+					search_string[1] = Integer.toString(page);
+					output = new HttpAsyncTask().execute(search_string).get();
+					if(page>=4)
+						btn_b.setEnabled(true);
+					makeview(listv);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// check if you are connected or not
+		if (isConnected()) {
+			tvIsConnected.setBackgroundColor(0xFF00CC00);
+			tvIsConnected.setText("You are conncted");
+		} else {
+			tvIsConnected.setText("You are NOT conncted");
+		}
+
+		makeview(listv);
+		return view;
+	}
+	
+	private void makeview(ListView listv){
+		// call AsynTask to perform network operation on separate thread
+				try {
+					JSONObject json = new JSONObject(output)
+							.getJSONObject("responseData");
+					JSONArray jarray = json.getJSONArray("results");
+					values_name = new String[jarray.length()];
+					values_desc = new String[jarray.length()];
+					for (int i = 0; i < jarray.length(); i++) {
+						String name = jarray.getJSONObject(i).getString(
+								"titleNoFormatting");
+						String desc = jarray.getJSONObject(i).getString("content");
+						values_name[i] = name;
+						values_desc[i] = desc;
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+						this.getActivity(), R.layout.list_elem, R.id.name, values_name);
+				// ArrayAdapter<String> adapter2 = new
+				// ArrayAdapter<String>(this.getActivity(),R.layout.list_elem,
+				// R.id.desc,values_desc);
+				listv.setAdapter(adapter);
+				// listv.setAdapter(adapter2);
+	}
+
+	public static String GET(String my_search, String count) {
 		URL url = null;
-		StringBuilder builder=null;
+		StringBuilder builder = null;
 		try {
 			url = new URL(
-				    "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
-				    + "q=+developer&userip=USERS-IP-ADDRESS");
+					"https://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
+							+ "q=+" + my_search + "&newwindow=1&start=" + count);
 			URLConnection connection = url.openConnection();
 			String line;
 			builder = new StringBuilder();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			while((line = reader.readLine()) != null) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			while ((line = reader.readLine()) != null) {
 				builder.append(line);
 			}
-			JSONObject json = new JSONObject(builder.toString());
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		InputStream inputStream = null;
-		String result = "";
-		
+
 		return builder.toString();
-	}
-
-	private static String convertInputStreamToString(InputStream inputStream)
-			throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(
-				new InputStreamReader(inputStream));
-		String line = "";
-		String result = "";
-		while ((line = bufferedReader.readLine()) != null)
-			result += line;
-
-		inputStream.close();
-		return result;
-
 	}
 
 	public boolean isConnected() {
@@ -126,16 +197,15 @@ public class Fragment3 extends Fragment {
 	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
 		@Override
 		protected String doInBackground(String... urls) {
-			
-			return GET(urls[0]);
+
+			return GET(urls[0], urls[1]);
 		}
 
 		// onPostExecute displays the results of the AsyncTask.
 		@Override
 		protected void onPostExecute(String result) {
-			Toast.makeText(getActivity().getBaseContext(), "Received!",
-					Toast.LENGTH_LONG).show();
-			etResponse.setText(result);
+			super.onPostExecute(result);
+
 		}
 	}
 }
