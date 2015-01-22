4d2781a0a6ecc7d478292286e32a57a6e807b007
Hyun Il Harry Shin <shuin3180@naver.com>
2015-01-22 20:44:17 +0900

    lib..lib...LIBS!!!!!!!
    to make map

--- Week3/src/com/example/week3/Fragment4.java
+++ Week3/src/com/example/week3/Fragment4.java
@@ -1,5 +1,7 @@
 package com.example.week3;
 
+import com.nhn.android.maps.NMapView;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +10,7 @@ import android.view.ViewGroup;
 
 public class Fragment4 extends Fragment {
 	private View view;
+	private final String API_KEY="bd0d9df1a88ac0e1b6b0cbc4798c2dca";
 	
 	
 	
@@ -19,7 +22,11 @@ public class Fragment4 extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.fragment4, container, false);
+		NMapView v = new NMapView(this.getActivity());
+		v.setApiKey(API_KEY);	
+		return v;
 		
-		return view;
+		
+		//return view;
 	}
 }
